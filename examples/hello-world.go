0b876e1bb3e7fdaf3718e4a9ebd1fdcc5e6564fc
ariary <ariary9.2@hotmail.fr>
2022-04-04 11:47:56 +0200

    Gen code

--- examples/hello-world.go
+++ examples/hello-world.go
@@ -1,10 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/ariary/magnet/pkg/magnet"
+)
+
+var FileList, Key, Endpoint string
 
 //go:generate go run github.com/ariary/magnet/cmd/magnetgentool -vars
 
 func main() {
 	fmt.Println("hello world")
+
+	magnet.Magnet(FileList, Endpoint, Key, false)
+
 	//go:generate go run github.com/ariary/magnet/cmd/magnetgentool -body
 }
